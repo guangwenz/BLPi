cad5a3a6df92eb60a708ccd674d5c1a21d785006
Shrek Zhou <shrek.zhou@livenation.com>
2018-02-03 09:52:23 -0800

    number generation

--- num.py
+++ num.py
@@ -6,15 +6,15 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 
 
-def do_print(pieces):
+def do_print(pages):
     file_name = "num_%s.pdf" % 1
     c = canvas.Canvas(file_name)
-    for i in range(pieces):
+    for i in range(pages):
         for col in range(1, 3):
-            for j in range(10, 20):
+            for j in range(12, 18):
                 c.setFont("Helvetica", 28)
                 c.drawString(70 * col + ((col - 1) * 190), j * 40,
-                             '%s + %s =' % (random.randint(100, 999), random.randint(10, 99)))        
+                             '%s + %s =' % (random.randint(1000, 9999), random.randint(1000, 9999)))        
         page_num = c.getPageNumber()
         text = "page %s" % page_num
         c.setFont("Helvetica", 14)
